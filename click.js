998c6cc2521f0cdf7979b4e50dcc0c7b30d3c6f2
KhanutBJ <95537645+KhanutBJ@users.noreply.github.com>
2024-02-28 20:46:31 +0700

    Create click.js

---
+++ click.js
@@ -0,0 +1,7 @@
+// Get all div elements with the specified class and attributes
+var divElements = document.querySelectorAll('.jWCzBe.QRiHXd.idtp4e.u73Apc');
+
+// Click each div element
+divElements.forEach(function(divElement) {
+    divElement.click();
+});
